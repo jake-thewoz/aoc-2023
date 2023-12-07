ebc036b44d845f01b6ef22b234239cf1370a5ffd
Jakob <jake.thewoz@gmail.com>
2023-12-07 11:00:54 -0500

    Finally got part 2 done!

--- 7.py
+++ 7.py
@@ -125,3 +125,76 @@ for i in range(len(deck)):
     winnings += bid * (i+1)
 
 print(f'The total winnings for part one is {winnings}')
+
+# Part Two --------------------------------------------------------------
+
+# Okay, this isn't so bad. All we have to do now is modify our rankHand function
+# to account for these 'jokers wild' rules.
+
+def jRankHand(hand):
+    # If there's no J, we can call our other function
+    if 'J' not in hand:
+        return rankHand(hand)
+    elif 'J' in hand and len(set(hand)) == 1:
+        # this is a DEVIOUS exception
+        return rankHand(hand)
+
+    # We'll make a modified version of the hand, then just pass it to our
+    # normal ranking function
+    modifiedHand = hand[:]
+
+    # In order to find the most common card that isn't J, we need to make a hand
+    # that has J removed
+    handWithoutJ = [card for card in hand if card != 'J']
+    # Then we can find the best option for J to become
+    mostCommonCard = max(handWithoutJ, key=handWithoutJ.count)
+
+    # Then we replace J in our new hand with this option
+    modifiedHand = modifiedHand.replace('J', mostCommonCard)
+
+    # ...and simply call our old rankHand with the new hand
+    return rankHand(modifiedHand)
+
+def jRankCard(card):
+    if not card.isnumeric():
+        if card == 'T':
+            return 10
+        elif card == 'J':
+            return 1
+        elif card == 'Q':
+            return 12
+        elif card == 'K':
+            return 13
+        else: # This is the ace
+            return 14
+    else:
+        return int(card)
+
+# Now we need a modified compare, which will use this new jRankHand()
+
+def jCompare(x, y):
+    if jRankHand(x) < jRankHand(y):
+        return True
+    elif jRankHand(x) > jRankHand(y):
+        return False
+    else: # They must be equal
+        for c in range(5):
+            if jRankCard(x[c]) < jRankCard(y[c]):
+                return True
+            elif jRankCard(x[c]) > jRankCard(y[c]):
+                return False
+    
+    # If the hands are trully equal, I guess we return true?
+    # Actually, it doesn't seem to matter either way
+    return False
+
+# Here goes nothing! Hopefully this works first time :)
+quickSort(deck, jCompare)
+
+# Now we calculate the total winnings :)
+winnings = 0
+for i in range(len(deck)):
+    bid = int(deck[i][1])
+    winnings += bid * (i+1)
+
+print(f'The total winnings for part two is {winnings}')
